e0338bd928e26940eaf8865de1e7a75ca54e0f45
Anhh <tranquangtuananh2801@gmail.com>
2025-01-29 14:44:31 +0700

    Update vite.config.ts

--- foobooloo-challenge/vite.config.ts
+++ foobooloo-challenge/vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/foobooloo-challenge/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
