420ccaca85d6f2712111026e0cb78abc4c53c191
Anhh <tranquangtuananh2801@gmail.com>
2025-01-29 14:35:38 +0700

    Update vite.config.ts

--- foobooloo-challenge/vite.config.ts
+++ foobooloo-challenge/vite.config.ts
@@ -2,18 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  // server: {
-  //   host: "::",
-  //   port: 8080,
-  // },
-  plugins: [
-    react(),
-  ].filter(Boolean),
-  base:"/foobooloo-challenge",
+export default defineConfig({
+  plugins: [react()],
+  base: "/foobooloo-challenge/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
